4653f3511ceb14a6330e8df614f0f7b345d46ebb
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2025-05-23 22:36:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -321,5 +321,6 @@ def search():
         if filepath and os.path.exists(filepath):
             safe_remove_file(filepath)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+# if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=10000)
+app.run(debug=True)
